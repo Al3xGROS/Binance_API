03033a2478acb299416762b4987314e162b111c9
Al3xGROS <al.gros@sfr.fr>
2022-12-25 17:17:25 +0100

    Task list - GET (2) - Part2

--- README.md
+++ README.md
@@ -1,42 +1,41 @@
 # Coinbase_API
 
-<h2>Prerequisites</h2>
-<p>- Use Python3 or js</p>
-<p>- No precompiled trading module, write the REST calls yourself using a rest module</p>
-<p>- Use Binance or Coinbase</p>
-<p>- Create a function for each task</p>
-<p>- Do not store your credentials on your GitHub!</p>
-<br/>
-<br/>
-
-<h2>Tasks list - GET  ✅</h2>
-<p>- Create a git repository and share it with the teacher ✅</p>
-<p>- Get a list of all available cryptocurrencies and display it ✅</p>
-<p>- Create a function to display the 'ask' or 'bid' price of an asset ✅</p>
-<p>- Get order book of an asset ✅</p>
-<br/>
-<br/>
-
-<h2>Tasks list - GET</h2>
-<p>- Create a function to read agregated trading data (candles)</p>
-<p>- Create a sqlite table to store said data</p>
-<p>- Store candlModify function to update when new candle data is available</p>
-<p>- e data in db</p>
-<p>- Create a function to extract all available trade data</p>
-<p>- Store the data in sqlite</p>
-<br/>
-<br/>
-
-<h2>Tasks list - POST</h2>
-<p>- Create an order</p>
-<p>- Cancel an order</p>
-<br/>
-<br/>
+##Prerequisites
+- Use Python3 or js
+- No precompiled trading module, write the REST calls yourself using a rest module
+- Use Binance or Coinbase
+- Create a function for each task
+- Do not store your credentials on your GitHub!
 
 
-<h2>References</h2>
-<p>Wikipedia page for APIs (https://en.wikipedia.org/wiki/API)</p>
-<p>Using requests in Python (https://www.pythonforbeginners.com/requests/using-requests-in-python)</p>
-<p>Binance API Documentation (https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md)</p>
-<p>Coinbase API Documentation (https://docs.cloud.coinbase.com/exchange/docs)</p>
+
+##Tasks list - GET  ✅
+ - Create a git repository and share it with the teacher ✅ 
+ - Get a list of all available cryptocurrencies and display it ✅ 
+ - Create a function to display the 'ask' or 'bid' price of an asset ✅ 
+ - Get order book of an asset ✅ 
+
+
+
+##Tasks list - GET  ✅
+ - Create a function to read agregated trading data (candles)  ✅
+ - Create a sqlite table to store said data ✅
+ - Store candle data in the database ✅
+ - Modify refreshDataCandle() to update when new candle data is available 
+ - Create a function to extract all available trade data ✅
+ - Store the data in sqlite ✅
+
+
+
+##Tasks list - POST
+ - Create an order 
+ - Cancel an order 
+
+
+
+##References
+ Wikipedia page for APIs (https://en.wikipedia.org/wiki/API) 
+ Using requests in Python (https://www.pythonforbeginners.com/requests/using-requests-in-python) 
+ Binance API Documentation (https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md) 
+ Coinbase API Documentation (https://docs.cloud.coinbase.com/exchange/docs) 
 
--- coinbase_api.py
+++ coinbase_api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sqlite3
+import ast
 
 # LIST OF ALL AVAILABLE CRYPTOCURRENCIES
 def getAllCrypto():
@@ -34,22 +35,75 @@ def getOrderBook(asset):
     print(response.text)
 
 
-# FUNCTION TO READ CANDLES (FOR A DURATION OF 5 MINUTES => DURATION = 300)
+# SQLITE TABLE TO STORE DATA FROM FUNCTION ABOVE 
+def storeData(data, table):
+    # Connect to the database
+    conn = sqlite3.connect("test.db")
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    # Create the table (if it doesn't already exist)
+    if table == "dataCandles":    
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS dataCandles(id INTEGER PRIMARY KEY, date INTEGER, high REAL, low REAL, open REAL, close REAL, volume REAL)"
+        )
+
+        # The list of data
+        newData = ast.literal_eval(data)
+
+        # Insert the data into the table
+        for row in newData:
+            cursor.execute(
+                "INSERT INTO dataCandles(date, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?)",
+                row,
+            )
+    elif table == "fullDataSet":
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS fullDataSet(id INTEGER PRIMARY KEY, uuid TEXT, traded_crypto REAL, price REAL, created_at_int INT, side TEXT)"
+        )
+
+        # The list of data
+        # newData = ast.literal_eval(data)
+
+        # Insert the data into the table
+        for record in data:
+            cursor.execute(
+                "INSERT INTO fullDataSet(uuid, traded_crypto, price, created_at_int, side) VALUES (?, ?, ?, ?, ?)",
+                (record['trade_id'], record['size'], record['price'], record['time'], record['side']),
+            )
+
+    # Commit the transaction
+    conn.commit()
+
+    # Close the connection
+    conn.close()
+
+
+# FUNCTION TO READ CANDLES (PAIR FORMAT EXAMPLE : "BTC-USD" ; FOR A DURATION OF 5 MINUTES => DURATION = 300)
 def refreshDataCandle(pair, duration):
     url = "https://api.exchange.coinbase.com/products/" + pair + "/candles?granularity=" + str(duration)
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
-    print(response.text)
+    # print(response.text)
+    storeData(response.text, "dataCandles")
 
 
-# SQLITE TABLE 
-def connectTable():
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS dataCandles
-                (Id INTEGER PRIMARY KEY, date INT, high REAL, low REAL, open REAL, close REAL, volume REAL)''')
+# MODIFY REFRESHDATACANDLE() TO UPDATE WHEN NEW CANDLE DATA IS AVAILABLE
+# def refreshDataCandle2(pair, duration):
+#     url = "https://api.exchange.coinbase.com/products/" + pair + "/candles?granularity=" + str(duration)
+#     headers = {"accept": "application/json"}
+#     response = requests.get(url, headers=headers)
+#     storeDataCandle(response.text)
 
-    connection.commit()
-    connection.close()
 
-connectTable()
+# FUNCTION TO EXTRACT ALL AVAILABLE TRADE DATA
+def refreshData(pair):
+    url = "https://api.exchange.coinbase.com/products/" + pair + "/trades"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    # print(response)
+    # print(type(response))
+    data = json.loads(response.text)
+    storeData(data, "fullDataSet")
+
